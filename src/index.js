9671bf132befae19ee52ce0c764062ff69415816
ribeirober1208 <ribeirober1208@gmail.com>
2023-07-28 17:17:41 -0300

    validacao2807

--- src/index.js
+++ src/index.js
@@ -1,3 +1,52 @@
 import validator from './validator.js';
 
+
 console.log(validator);
+// estudar material variáveis console. https://curriculum.laboratoria.la/pt/topics/javascript/basics/variables
+// mensagem de return
+
+document.getElementById("enviar").addEventListener("click",validarCard)
+document.getElementById("enviar").addEventListener("click",validarNome)
+document.getElementById("enviar").addEventListener("click",validarData)
+document.getElementById("enviar").addEventListener("click",validarCvv)
+document.getElementById('numero').addEventListener("input",maskify)
+
+function validarCard(event) {
+    event.preventDefault()
+    const validarCard = document.getElementById("numero").value;
+    console.log(validarCard)
+    const validado = validator.isValid(validarCard)
+    if (validado){
+        alert("Seu cartão é válido")
+    }else{
+        alert('Seu cartão é inválido')
+
+    }
+}
+function validarNome(event) {
+    event.preventDefault()
+    const validarNome = document.getElementById("nome").value;
+    console.log(validarNome)
+    
+}
+function validarData(event) {
+    event.preventDefault()
+    const validarData = document.getElementById("validade").value;
+    console.log(validarData)
+   
+}
+function validarCvv(event) {
+    event.preventDefault()
+    const validarCvv = document.getElementById("cvv").value;
+    console.log(validarCvv)
+   
+}
+
+function maskify(event) {
+    const cartaoMascarado = document.getElementById("mascarado");
+    const numeroCard = document.getElementById("numero").value;
+    const numeroMascarado = validator.maskify(numeroCard)
+    cartaoMascarado.innerHTML= numeroMascarado
+    console.log('teste');
+    
+}
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,53 @@
 const validator = {
-  // ...
+
+  // estudar array, funcion https://curriculum.laboratoria.la/pt/topics/javascript/arrays/arrays
+
+  isValid: function (validarCard) {
+
+    if (validarCard.length !== 16) {
+      return false;
+    }
+    // array de números de manipulação
+    const cardNumbers = validarCard.split('').map(Number);
+    // loop
+    for (let i = 0; i < cardNumbers.length; i += 2) {
+      cardNumbers[i] *= 2;
+      // se o número for maior que 9
+      if (cardNumbers[i] > 9) {
+        cardNumbers[i] -= 9;
+
+
+      }
+      //somar os números
+      const sum = cardNumbers.reduce((acc, curr) => acc + curr, 0);
+      //se o resultador for multiplo de 10
+      if (sum % 10 === 0) {
+
+        return true;
+
+      } else {
+        return false;
+      }
+
+
+    }
+
+  },
+
+  maskify: function (numero) {
+
+    if (numero.length > 4) {
+
+      const last4Digits = numero.slice(-4);
+      const mask = "*".repeat(numero.length - 4);
+      return mask + last4Digits;
+
+    } else {
+      return numero;
+    }
+
+  }
+
 };
 
-export default validator;
+export default validator
