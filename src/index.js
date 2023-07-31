4cdf0e164743ab0b7a04e88e0c4f3ce7fa296f38
ribeirober1208 <ribeirober1208@gmail.com>
2023-07-30 21:30:38 -0300

    atualizacao3007

--- src/index.js
+++ src/index.js
@@ -2,51 +2,64 @@ import validator from './validator.js';
 
 
 console.log(validator);
-// estudar material variáveis console. https://curriculum.laboratoria.la/pt/topics/javascript/basics/variables
-// mensagem de return
+// estudar material variáveis https://curriculum.laboratoria.la/pt/topics/javascript/basics/variable
 
-document.getElementById("enviar").addEventListener("click",validarCard)
-document.getElementById("enviar").addEventListener("click",validarNome)
-document.getElementById("enviar").addEventListener("click",validarData)
-document.getElementById("enviar").addEventListener("click",validarCvv)
-document.getElementById('numero').addEventListener("input",maskify)
+document.getElementById("enviar").addEventListener("click", validarCard)
+document.getElementById("enviar").addEventListener("click", validarNome)
+document.getElementById("enviar").addEventListener("click", validarData)
+document.getElementById("enviar").addEventListener("click", validarCvv)
+document.getElementById('numero').addEventListener("input", maskify)
+document.getElementById('cvv').addEventListener("input", validarCvv)
+document.getElementById('nome').addEventListener("input", validarNome)
+document.getElementById('validade').addEventListener("input", validarData)
 
 function validarCard(event) {
     event.preventDefault()
     const validarCard = document.getElementById("numero").value;
     console.log(validarCard)
     const validado = validator.isValid(validarCard)
-    if (validado){
+    if (validado) {
         alert("Seu cartão é válido")
-    }else{
+    } else {
         alert('Seu cartão é inválido')
 
     }
 }
+
 function validarNome(event) {
     event.preventDefault()
-    const validarNome = document.getElementById("nome").value;
-    console.log(validarNome)
-    
+    const nomeCliente = document.getElementById("nomeCartao");
+    const textoNome = document.getElementById("nome").value;
+    const mostrarNome = validator.validarNome(textoNome);
+    nomeCliente.textContent = mostrarNome
+    console.log('validarNome')
+
 }
 function validarData(event) {
     event.preventDefault()
-    const validarData = document.getElementById("validade").value;
-    console.log(validarData)
-   
+    const cartaoCvv = document.getElementById("dataValidade");
+    const numeroData = document.getElementById("validade").value;
+    const mostrarData = validator.validarData(numeroData)
+    cartaoCvv.innerHTML = mostrarData
+    console.log('validarData')
+
 }
 function validarCvv(event) {
     event.preventDefault()
-    const validarCvv = document.getElementById("cvv").value;
-    console.log(validarCvv)
-   
+    const cartaoCvv = document.getElementById("numeroCvv");
+    const numeroCvv = document.getElementById("cvv").value;
+    const mostrarCvv = validator.validarCvv(numeroCvv)
+    cartaoCvv.innerHTML = mostrarCvv
+    console.log('validarCvv')
+
 }
 
 function maskify(event) {
     const cartaoMascarado = document.getElementById("mascarado");
     const numeroCard = document.getElementById("numero").value;
     const numeroMascarado = validator.maskify(numeroCard)
-    cartaoMascarado.innerHTML= numeroMascarado
-    console.log('teste');
-    
+    cartaoMascarado.innerHTML = numeroMascarado
+    console.log('mascara');
+
 }
+
--- src/validator.js
+++ src/validator.js
@@ -7,20 +7,20 @@ const validator = {
     if (validarCard.length !== 16) {
       return false;
     }
-    // array de números de manipulação
+    
     const cardNumbers = validarCard.split('').map(Number);
-    // loop
+   
     for (let i = 0; i < cardNumbers.length; i += 2) {
       cardNumbers[i] *= 2;
-      // se o número for maior que 9
+    
       if (cardNumbers[i] > 9) {
         cardNumbers[i] -= 9;
 
 
       }
-      //somar os números
+      
       const sum = cardNumbers.reduce((acc, curr) => acc + curr, 0);
-      //se o resultador for multiplo de 10
+
       if (sum % 10 === 0) {
 
         return true;
@@ -46,8 +46,42 @@ const validator = {
       return numero;
     }
 
-  }
+  },
 
+  validarCvv: function (numeroCvv) {
+
+    if (/^[0-9]+$/.test(numeroCvv)) {
+      return numeroCvv;
+    } else {
+      return null;
+    }
+  },
+
+  validarData: function (numeroData) {
+    
+    if (/^\d{2}\/\d{4}$/.test(numeroData)) {
+      const [mes, ano] = numeroData.split('/');
+      const mesInt = parseInt(mes, 10);
+      const anoInt = parseInt(ano, 10);
+
+      if (mesInt >= 1 && mesInt <= 12 && anoInt >= 1900) {
+        return numeroData;
+      } else {  
+        resultado.mensagem = "Formato Inválido"
+      }
+    }
+  },
+
+  validarNome: function (nomeCliente) {
+  
+    if (/^[a-zA-ZÀ-ÿ\s]+$/.test(nomeCliente)) {
+      return nomeCliente;
+    }
+  
+    return ""; 
+  }
+  
+  
 };
 
 export default validator
